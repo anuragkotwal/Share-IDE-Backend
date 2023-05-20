898472ed606eb2f1a49829a8d35f32dcec62ce14
anurag-nomoex <anurag.kotwal@nomoex.in>
2023-05-20 14:50:01 +0530

    error fixed

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const logger = require('./utils/logger')
 const socket = require('./socket/socket')
 
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server,{
+    cors: {
+        origin: [/http:\/\/localhost:[0-9]{4}/,process.env.FRONTEND_URL],
+        methods: ['GET','POST'],
+    },
+})
 socket(io)
 
 app.use(function (req, res, next) {
